6013ba989d446767537642607bcd78230205542e
David Chaiken <chaiken@altiscale.com>
2015-05-02 09:03:57 -0700

    EP-143: default provides in metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -5,3 +5,10 @@ license          'All rights reserved'
 description      'Installs/Configures alti_skeleton'
 long_description long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          IO.read(File.join(File.dirname(__FILE__), 'VERSION'))
+
+{
+  'alti_skeleton::default' => 'Installs/Configures alti_skeleton'
+}.each do |rec, desc|
+  provides rec
+  recipe rec, desc
+end
