4d0149b32d7b74c686616a9fe45fef2d9c7c97a9
David Chaiken <chaiken@altiscale.com>
2015-05-02 09:16:35 -0700

    (Re)fixed rubocop issues

--- metadata.rb
+++ metadata.rb
@@ -1,10 +1,11 @@
-name             'alti_skeleton'
-maintainer       'YOUR_NAME'
+name 'alti_skeleton'
+maintainer 'YOUR_NAME'
 maintainer_email 'YOUR_EMAIL'
-license          'All rights reserved'
-description      'Installs/Configures alti_skeleton'
-long_description long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          IO.read(File.join(File.dirname(__FILE__), 'VERSION'))
+license 'All rights reserved'
+description 'Installs/Configures alti_skeleton'
+long_description long_description IO.read(File.join(File.dirname(__FILE__),
+                                                    'README.md'))
+version IO.read(File.join(File.dirname(__FILE__), 'VERSION'))
 
 {
   'alti_skeleton::default' => 'Installs/Configures alti_skeleton'
